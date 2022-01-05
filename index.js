e199d50d0f8f1462699c7f8dfb618faca9ed7f5e
= <rtyenriques@gmail.com>
2022-01-05 00:55:27 -0800

    commit tree a41eda0ec2bb268868bcfb63264c2477e108eae2

--- index.js
+++ index.js
@@ -13,5 +13,11 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+      const tutorialsNew = tutorials.map
+      (tutorial => tutorial.split(' ')
+      );
+      return tutorialsNew.join(' ')
 }
+      
+      
+
